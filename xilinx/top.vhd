2af5ae57d297efa720f86dce0df0465fe74cf642
Giovanni Funchal <gafunchal@gmail.com>
2011-01-03 12:09:01 +0100

    debug lcd

--- xilinx/top.vhd
+++ xilinx/top.vhd
@@ -41,6 +41,7 @@ architecture behavioral of top is
 
 	component dcm32to28 is
         port(
+            RST_IN          : IN  std_logic;
             CLKIN_IN        : IN  std_logic;          
             CLKFX_OUT       : OUT std_logic;
             CLKIN_IBUFG_OUT : OUT std_logic;
@@ -48,8 +49,21 @@ architecture behavioral of top is
         );
 	end component;
 
+    component deltasigmadac is
+        generic(
+            width  : integer := 8
+        );
+        port(
+            reset  : in  std_logic;
+            clock  : in  std_logic;
+            dacin  : in  std_logic_vector(width-1 downto 0);
+            dacout : out std_logic
+        );
+    end component;
+
     signal reset       : std_logic;
     signal pixel_clock : std_logic;
+    signal audio_clock : std_logic;
     signal x           : std_logic_vector(9 downto 0);
     signal y           : std_logic_vector(8 downto 0);
     signal red         : std_logic_vector(5 downto 0);
@@ -57,10 +71,13 @@ architecture behavioral of top is
     signal blue        : std_logic_vector(5 downto 0);
     signal hflip       : std_logic;
     signal vflip       : std_logic;
+    signal audioin     : std_logic_vector(7 downto 0);
+    signal mult        : std_logic_vector(1 downto 0);
 begin
 
     dcm32to28_0 : dcm32to28
         port map(
+            RST_IN => reset,
             CLKIN_IN => clock,
             CLKFX_OUT => pixel_clock,
             CLKIN_IBUFG_OUT => open,
@@ -99,18 +116,51 @@ begin
             y         => y
         );
 
-    red <= (others => x(5));
-    green <= (others => '1');
-    blue <= (others => y(5));
-
+    red <= x(5 downto 0);
+    green <= (others => x(5) and y(3));
+    blue <= y(8 downto 3);
+    
     reset <= '0';
 
     hflip <= '1';
     vflip <= '1';
-    
+
     wingBH(5) <= hflip;
     wingAL(2) <= vflip;
 
     wingBL(2) <= pixel_clock;
 
+    process(pixel_clock, reset)
+    begin
+        if (reset = '1') then
+            audioin <= (others => '0');
+            mult <= (others => '0');
+        elsif rising_edge(pixel_clock) then
+            audioin <= audioin * mult;
+            if(audioin > b"11111100") then
+                mult <= mult + 1;
+            end if;
+        end if;
+    end process;
+
+    deltasigmadac_0 : deltasigmadac
+        port map(
+            reset  => reset,
+            clock  => pixel_clock,
+            dacin  => audioin,
+            dacout => wingAH(6)
+        );
+
+    -- tip/left/mono channel
+    -- audiotip <= wingAH(6);
+
+    -- ring/right channel
+    -- audioring <= wingAH(7);
+
+    -- connected to tip when not plugged in, otherwise open
+    -- tipswitch <= wingBL(0);
+    
+    -- connected to ring when mono, otherwise open
+    -- ringswitch <= wingBL(1);
+    
 end architecture;
